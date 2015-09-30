1285cc0fbc14d3063bf2c1826b2eaf95e75ee340
Vincent Driessen <me@nvie.com>
2015-09-30 22:57:48 +0200

    Add dock orientation

--- osx-settings.sh
+++ osx-settings.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Dock
-# ...
+defaults write com.apple.dock orientation -string "left"
+killall Dock
 
 # Show battery percentage
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
